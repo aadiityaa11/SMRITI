e6274da34e0df22471609572d46c0f4208676333
Aditya <gaditya110803@gmail.com>
2024-07-07 12:22:30 +0530

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                    <span className='text-green-500'> SM</span>
+                    <span>RI</span><span className='text-green-500'>TI</span>
+                </div>
+        </div>
+  )
+}
+
+export default Footer
